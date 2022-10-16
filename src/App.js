11c6c96a648655ba66eb608fa579d5ad9cdc6c12
Myudro33 <qanashvili.myudro@gmail.com>
2022-10-16 20:38:10 +0400

    set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex flex-col">
+      <div className="w-full h-1/2 bg-red-300">
+        
+      </div>
+      <div className="w-full h-1/2 bg-blue-100"></div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      'xs': '375px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1440px',
+      // => @media (min-width: 1280px) { ... }
+    },
+  },
+  plugins: [],
+}
